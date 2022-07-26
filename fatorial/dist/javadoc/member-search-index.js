5e38ec35fe92fe2133f1a339e18778e2f3a7a531
Mateus Lopes <96300352+Mateusvll@users.noreply.github.com>
2022-07-26 08:30:25 -0300

    Add files via upload

---
+++ fatorial/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Fatorial","l":"Fatorial()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Fatorial","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ fatorial/src/Fatorial.java
@@ -0,0 +1,13 @@
+public class Fatorial {
+public static void main (String args[]){
+
+int x = 10; 
+int f = x; 
+
+while (x > 1){
+  f = f *(x-1); x--;
+                    }
+  System.out.println(f); // Aqui exibira o valor final do Fatorial 
+                                                         }
+                                    } 
+                                    
